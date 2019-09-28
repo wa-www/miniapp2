63678b7d8c939f247b81b1e19bf585364f23cac4
Wa-www <0nicoco0@gmail.com>
2019-09-29 03:02:44 +0900

    initial commit
    view-materialize-scssの導入まで

---
+++ db/migrate/20190928092358_create_blogs.rb
@@ -0,0 +1,10 @@
+class CreateBlogs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :blogs do |t|
+      t.string      :name
+      t.text :title
+      t.text        :text
+      t.timestamps null: true
+    end
+  end
+end
